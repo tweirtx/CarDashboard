fbfeeaf9c0a96f3a6ca651b7521d7f69ca796933
Travis Weir <tweirtx@gmail.com>
2022-03-28 22:20:37 -0500

    OBDII interface code

--- CarDashboard.py
+++ CarDashboard.py
@@ -1,2 +1,13 @@
 import pygame
 import obd
+
+connection = obd.OBD()
+# pygame init stuff
+
+while True:
+    # get values from the car
+    coolant_temp = connection.query(obd.commands.COOLANT_TEMP).value
+    fuel_level = connection.query(obd.commands.FUEL_LEVEL).value
+    battery_voltage = connection.query(obd.commands.CONTROL_MODULE_VOLTAGE).value
+
+    # update pygame fields
